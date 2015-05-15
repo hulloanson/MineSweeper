9b742f9e6a8634b1980bb78c1724531265fe4760
hulloanson <hulloanson@gmail.com>
2015-05-16 01:02:42 +0800

    Finished.
    Finished without result recording, mass clearing of tiles.

--- MineSweeper/ViewController.swift
+++ MineSweeper/ViewController.swift
@@ -16,6 +16,23 @@ class ViewController: UIViewController {
     let boardSize:Int = 10
     var board:Board
     var squareButtons:[SquareButton] = []
+    var movesCount = 0
+    var secondCount = 0
+    var firstMove = true
+    
+    var oneSecondTimer:NSTimer?
+    
+    func oneSecond() {
+        secondCount++
+        timeLabel.text = "Time: \(secondCount)"
+    }
+    
+    func endCurrentGame() {
+        if self.oneSecondTimer != nil {
+            self.oneSecondTimer!.invalidate()
+            self.oneSecondTimer = nil
+        }
+    }
     
     required init(coder aDecoder: NSCoder) {
         self.board = Board(size: boardSize)
@@ -29,7 +46,7 @@ class ViewController: UIViewController {
                 let squareSize:CGFloat = self.boardView.frame.width / CGFloat(boardSize)
                 let squareButton = SquareButton(squareModel: square, squareSize: squareSize, squareMargin: 0)
                 squareButton.setTitleColor(UIColor.darkGrayColor(), forState: .Normal)
-                squareButton.addTarget(self, action: "pressed:", forControlEvents: .TouchUpInside)
+                squareButton.addTarget(self, action: "squareButtonPressed:", forControlEvents: .TouchUpInside)
                 self.boardView.addSubview(squareButton)
                 self.squareButtons.append(squareButton)
             }
@@ -45,12 +62,66 @@ class ViewController: UIViewController {
     
     func uiStartNewGame() {
         println("UI reset called.")
+        movesCount = 0
+        movesLabel.text = "Moves: \(movesCount)"
+        secondCount = 0
+        timeLabel.text = "Time: \(secondCount)"
         self.resetBoardView()
+        
+
     }
     
     func squareButtonPressed(sender: SquareButton) {
+        if firstMove {
+            self.oneSecondTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("oneSecond"), userInfo: nil, repeats: true)
+            firstMove = false
+        }
         println("Pressed row:\(sender.square.row), col:\(sender.square.column)")
-//        sender.setTitle("", forState: .Normal)
+        var setText = decideSquareButtonText(sender.square)
+        switch setText {
+        case "pressedAlredy":
+            println(setText)
+        case "M":
+            minePressed();
+        default:
+            sender.setTitle(setText, forState: .Normal)
+        }
+    }
+    
+    func minePressed(){
+        self.endCurrentGame()
+        var alertView = UIAlertView()
+        alertView.addButtonWithTitle("New Game")
+        alertView.title = "OOPS."
+        alertView.message = "You tapped on a mine."
+        alertView.show()
+        alertView.delegate = self
+    }
+    
+    func alertView(View: UIAlertView!, clickedButtonAtIndex buttonIndex: Int) {
+        //start new game when the alert is dismissed
+        self.uiStartNewGame()
+    }
+    
+    func decideSquareButtonText(square: Square) -> String {
+        var returnText = ""
+        if square.isRevealed {
+            returnText = "pressedAlready"
+        } else {
+            movesCount++
+            movesLabel.text = "Moves: \(movesCount)"
+            square.isRevealed = true;
+            if square.isMine {
+                returnText = "M"
+            } else {
+                if square.minedNeighborCount > 0 {
+                    returnText = "\(square.minedNeighborCount)"
+                } else {
+                    returnText = ""
+                }
+            }
+        }
+        return returnText
     }
     
     override func viewDidLoad() {
@@ -66,8 +137,10 @@ class ViewController: UIViewController {
     }
     
     @IBAction func newGamePressed(){
+        self.endCurrentGame()
         println("New Game.")
         self.uiStartNewGame()
+
     }
 
 
